259e3e803e90f24a4c7ec412009b5e0754141166
AdamiMath <matheusadami.lima@gmail.com>
2023-12-04 14:12:22 -0300

    adicionado outro repositório

--- src/views/exemples/About.jsx
+++ src/views/exemples/About.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-
-const About = poprs => 
-
-(
-<div className="About"><h1>Sobre</h1><p>O nosso sistema foi criado</p></div>
+const About = (poprs) => (
+  <div className="About">
+    <h1>Sobre</h1>
+    <p>Nosso sistema</p>
+  </div>
 );
 
 export default About;
